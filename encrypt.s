dbc010ae0582773381199003f9de0e955c1c6d74
kranonetka <fakemail4steam@ya.ru>
2017-09-19 12:32:25 +0700

    Added invisible key typing

--- encrypt.s
+++ encrypt.s
@@ -3,7 +3,7 @@ section .data
 	filename: db "filename: "
 	filename_len: equ $-filename
 section .bss
-	descriptor: resb 4
+	terminal_params: resb 36
 	buffer: resb 4096
 	buffer_size: equ $-buffer
 	key: resb 80
@@ -19,23 +19,21 @@ _start:
 
 	xor eax,eax
 	times 3 inc eax
-	dec ebx
+	xor ebx,ebx
 	mov ecx,buffer
 	mov edx,buffer_size
 	int 0x80
-	and [buffer+eax-1], byte 0
+	and [buffer+eax-1], byte 0	;replace enter to '\0'
 
 	mov eax,5
 	mov ebx,buffer
 	xor ecx,ecx
 	int 0x80
-
 	push eax
 
 	xor eax,eax
 	times 3 inc eax
-	pop ebx
-	push ebx
+	mov ebx,dword [esp]
 	mov ecx,buffer
 	mov edx,buffer_size
 	int 0x80
@@ -45,6 +43,36 @@ _start:
 	xor ebx,ebx
 	inc ebx
 	mov ecx,buffer
+	int 0x80
+
+	mov eax,54
+	xor ebx,ebx
+	mov ecx,0x5401
+	mov edx,terminal_params
+	int 0x80
+	
+	xor [terminal_params+12],byte 8
+	
+	mov eax,54
+	inc ecx
+	int 0x80
+	
+	xor eax,eax
+	times 3 inc eax
+	mov ecx,key
+	mov edx,key_size
+	int 0x80
+
+	mov edx,eax
+	mov eax,4
+	inc ebx
+	int 0x80
+
+	xor [terminal_params+12],byte 8
+	mov eax,54
+	xor ebx,ebx
+	mov ecx,0x5402
+	mov edx,terminal_params
 	int 0x80
 
 	mov eax,6

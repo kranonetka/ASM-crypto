8e56d983704549e8df182fb434c1370a1bf3a039
kranonetka <fakemail4steam@ya.ru>
2017-10-20 16:22:04 +0700

    [+] Something

--- substrsearch.s
+++ substrsearch.s
@@ -1,94 +1,165 @@
 global _start
 section .data
-	string: db "string: "
-	string_len: equ $-string
-	substring: db "substring for search: "
-	substring_len: equ $-substring
+	msg_string: db "string: "
+	msg_string_len: equ $-msg_string
+	msg_substring: db "substring for search: "
+	msg_substring_len: equ $-msg_substring
 section .bss
 	input_str: resb 4096
 	input_str_size: equ $-input_str
 	input_substr: resb 4096
 	input_substr_size: equ $-input_substr
+	string_len: resd 1
+	substring_len: resd 1
 section .text
 _start:
-	mov esi,input_str
-	mov edi,input_substr
 
 	mov eax,4
 	xor ebx,ebx
 	inc ebx
-	mov ecx,string
-	mov edx,string_len
+	mov ecx,msg_string
+	mov edx,msg_string_len
 	int 0x80
 
 	xor eax,eax
 	times 3 inc eax
 	xor ebx,ebx
-	mov ecx,esi
+	mov edi,input_str
+	mov ecx,edi
 	mov edx,input_str_size
+	int 0x80
+	dec eax
+	and byte [edi+eax],0
+
+;;;;;;;;TEST
+	call strlen
+	mov eax,ecx
+	call printReg
+	mov eax,1
+	mov ebx,0
+	int 0x80
+;;;;;;;;
+
+
+	
+	mov eax,4
+	inc ebx
+	mov ecx,msg_substring
+	mov edx,msg_substring_len
+	int 0x80
+
+	xor eax,eax
+	times 3 inc eax
+	xor ebx,ebx
+	mov esi,input_substr
+	mov ecx,esi
+	mov edx,input_substr_size
 	int 0x80
 	dec eax
 	and byte [esi+eax],0
 	
-	mov eax,4
-	inc ebx
-	mov ecx,substring
-	mov edx,substring_len
-	int 0x80
+	;edi - string
+	;esi - substring
+	call substrsearch
 
-	xor eax,eax
-	times 3 inc eax
-	xor ebx,ebx
-	mov ecx,edi
-	mov edx,input_substr_size
-	int 0x80
-	dec eax
-	and byte [edi+eax],0
-	
-	;call substrsearch
+	;eax - start pos of match (-1 if no match)
+	call printReg
 
 	xor eax,eax
 	inc eax
+	xor ebx,ebx
 	int 0x80
 
-substrsearch:
-;esi - source string
-;edi - substring for search
-	push edi
+strlen:
+;edi - string address
+push eax
 	xor eax,eax
 	xor ecx,ecx
 	dec ecx
 	cld
-	repne scasb	;compare with al=0
+	repne scasb
 	neg ecx
-	mov edx,ecx	;edx - substrnig length
+pop eax
+	ret
 
+
+substrsearch:
+;edi - source string
+;esi - substring for search
+push edi
+push esi
+push ecx
+	xor eax,eax
+	xor ecx,ecx
+	dec ecx ;ecx - max 32bit value
+	cld
+push edi
+	repne scasb ;compare with '\0'
+pop edi
+	neg ecx ;ecx - count of symbols in string
+	mov [string_len],ecx
 	xor ecx,ecx
 	dec ecx
+push edi
 	mov edi,esi
-	repne scasb	;compare with al=0
-	neg ecx	;ecx - src string length
-	
-	pop edi
+	repne scasb
+pop edi
+	neg ecx ;ecx - count of symbols in substring
+	dec ecx
+	mov [substring_len],ecx
 
-	;esi - src string
-	;edi - substring
-	;ecx - src string length
-	;edx - substring length
-	
-	cmp edx,ecx	;substr len ? srcstr len
-	jle length_ok	;if substring length less or equal
-			;than src string length
-	
+	;edi - string address
+	;esi - substring address
+
+	mov edx,edi ;save string address
+
+	lodsb ;read first symbol from substring (esi)
+	mov ebx,esi ;address of second symbol
+._loop:
+	mov esi,ebx
+	mov ecx,[string_len]
+	repne scasb
+	jz ._mb_found
+pop ecx
+pop esi
+pop edi
+	xor eax,eax
 	dec eax
-	ret	;if substr len greater than
-		;src string len return eax=-1
-length_ok:
-	push edi
-	push esi
+	ret
+._mb_found:
+	mov [string_len],ecx
+	mov ecx,[substring_len]
+	repe cmpsb
+	jnz ._loop
+	sub edi,edx
+	sub edi,[substring_len]
+	mov eax,edi
+pop ecx
+pop esi
+pop edi
+ret
 
-	mov byte al,[edi] ;al contains first symbol from substr
-
-	xchg esi,edi
-	;esi - substring
-	;edi - src string
+printReg:
+;eax - register with value
+	xor ecx,ecx
+	mov bx,10
+	sub esp,14
+._stack_fill:
+	mov esi,esp
+	xor edx,edx
+	div bx
+	add dl,'0'
+	sub esi,ecx
+	mov [esi],dl
+	inc ecx
+	test eax,eax
+	jnz ._stack_fill
+	
+	mov edx,ecx
+	mov eax,4
+	mov ebx,1
+	mov ecx,esi
+	int 0x80
+	add esp,14
+ret
+	
